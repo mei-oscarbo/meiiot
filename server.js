af911265165f9a7175e145ae6ea70f65f2c248b8
mei-oscarbo <oscarbo@mei.es>
2024-10-28 11:35:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ io.on("connection", function (socket) {
     })
 
     socket.on("download", function (data) {
-	console.log("requesting "+data+" "file")
+	console.log("requesting "+data+" file")
 	socket.emit("download", data)
     })
 	ss(socket).on('file', function(stream) {
